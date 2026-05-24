b5f0cb7f32f154d089bb675709f580140dcf12aa
SharequeKhan77 <sharequekhan25@gmail.com>
2026-05-24 18:15:36 +0530

    Initial quiz app setup with models and index view

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    created_date = models.DateField(auto_now=True)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=30)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=30)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Quiz, Question, Answer
 
 # Create your views here.
+
+def index(request):
+    quizzes = Quiz.objects.all()
+    return render(request, 'quiz/index.html', {'quizzes': quizzes})
