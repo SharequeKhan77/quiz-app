0de8dad4e1cc57b7ea25ad6788984c837caf02ba
SharequeKhan77 <sharequekhan25@gmail.com>
2026-05-26 16:02:28 +0530

    Add quiz detail and submit functionality

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index') 
+    path('', views.index, name='index'),
+    path('detail/<int:id>/', views.view_detail, name='view_detail'),
+    path('submit/<int:id>/', views.submit_quiz, name='submit_quiz'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -6,3 +6,28 @@ from .models import Quiz, Question, Answer
 def index(request):
     quizzes = Quiz.objects.all()
     return render(request, 'quiz/index.html', {'quizzes': quizzes})
+
+def view_detail(request, id):
+    quiz = Quiz.objects.get(id=id)
+    questions = Question.objects.filter(quiz=quiz)
+    return render(request, 'quiz/detail.html', {'quiz': quiz, 'questions': questions})
+
+def submit_quiz(request, id):
+    if request.method == 'POST':
+        quiz = Quiz.objects.get(id=id)
+        questions = Question.objects.filter(quiz=quiz)
+        score = 0
+        total = questions.count()
+        
+        for question in questions:
+            selected_answer_id = request.POST.get(f'question_{question.id}')
+            if selected_answer_id:
+                answer = Answer.objects.get(id=selected_answer_id)
+                if answer.is_correct:
+                    score += 1
+        
+        return render(request, 'quiz/result.html', {
+            'score': score,
+            'total': total,
+            'quiz': quiz
+        })
