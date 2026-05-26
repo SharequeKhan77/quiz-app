8a5ced9ba0c5fc4f12f44af423ea89cd23cd63ac
SharequeKhan77 <sharequekhan25@gmail.com>
2026-05-26 16:59:56 +0530

    Add login authentication

--- quiz/views.py
+++ quiz/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from .models import Quiz, Question, Answer
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/login/')
 def index(request):
     quizzes = Quiz.objects.all()
     return render(request, 'quiz/index.html', {'quizzes': quizzes})
 
+@login_required(login_url='/login/')
 def view_detail(request, id):
     quiz = Quiz.objects.get(id=id)
     questions = Question.objects.filter(quiz=quiz)
     return render(request, 'quiz/detail.html', {'quiz': quiz, 'questions': questions})
 
+@login_required(login_url='/login/')
 def submit_quiz(request, id):
     if request.method == 'POST':
         quiz = Quiz.objects.get(id=id)
