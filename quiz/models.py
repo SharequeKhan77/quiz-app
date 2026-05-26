700f725858c5dd1c842e3391c8fad46c76718dcf
SharequeKhan77 <sharequekhan25@gmail.com>
2026-05-26 17:42:41 +0530

    Increase field max length for questions and answers

--- quiz/models.py
+++ quiz/models.py
@@ -8,10 +8,10 @@ class Quiz(models.Model):
     created_date = models.DateField(auto_now=True)
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=30)
+    question_text = models.CharField(max_length=200)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
 
 class Answer(models.Model):
-    answer_text = models.CharField(max_length=30)
+    answer_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
